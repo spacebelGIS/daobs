45346531df36addfe0b18e255206a84c05a15f09
François Prunayre <fxprunayre@gmail.com>
2014-10-20 13:37:45 +0200

    Add utility method to query Solr by URL (may be used from XSLT) or get num rows using Solrj.

--- core/src/main/java/org/daobs/index/SolrRequestBean.java
+++ core/src/main/java/org/daobs/index/SolrRequestBean.java
@@ -1,14 +1,28 @@
 package org.daobs.index;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrResponse;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.request.FieldAnalysisRequest;
+import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.client.solrj.response.AnalysisResponseBase;
 import org.apache.solr.client.solrj.response.FieldAnalysisResponse;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.params.AnalysisParams;
 import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
+import java.io.StringReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -23,6 +37,47 @@ public class SolrRequestBean {
     private static String PHASE_QUERY = "query";
     private static String DEFAULT_FILTER_CLASS = "org.apache.lucene.analysis.synonym.SynonymFilter";
 
+    public static Node query(String query) {
+        try {
+            SolrServerBean serverBean = SolrServerBean.get();
+            URL url = new URL(serverBean.getSolrServerUrl() + query);
+            String xmlResponse = IOUtils.toString(url);
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(xmlResponse));
+            return builder.parse(is).getFirstChild();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * Return the number of rows matching the query.
+     *
+     * @param query The query
+     * @return
+     */
+    public static long getNumFound(String query, String... filterQuery) {
+        try {
+            SolrQuery solrQuery = new SolrQuery();
+            solrQuery.setQuery(query);
+            if (filterQuery != null) {
+                solrQuery.setFilterQueries(filterQuery);
+            }
+            solrQuery.setRows(0);
+
+            SolrServerBean serverBean = SolrServerBean.get();
+            SolrServer solrServer = serverBean.getServer();
+
+            QueryResponse solrResponse = solrServer.query(solrQuery);
+            return solrResponse.getResults().getNumFound();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
     /**
      * Analyze a field and a value against the index phase
      * and return the first value generated
