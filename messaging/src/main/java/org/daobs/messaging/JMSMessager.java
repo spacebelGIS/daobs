008e3bfb0480e6a2c819c563fb4459e30e8aba80
Jose Garcia <jose.garcia@geocat.net>
2015-12-10 12:11:35 +0100

    #60 ETF validation service. Improve JMSMessanger cleanup and logging

--- messaging/src/main/java/org/daobs/messaging/JMSMessager.java
+++ messaging/src/main/java/org/daobs/messaging/JMSMessager.java
@@ -1,6 +1,8 @@
 package org.daobs.messaging;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationEvent;
 
 import javax.jms.*;
@@ -9,6 +11,9 @@ import javax.jms.*;
  * Created by francois on 05/11/15.
  */
 public class JMSMessager {
+    private Log log = LogFactory.getLog(this.getClass());
+
+
     private String jmsUrl;
 
     public String getJmsUrl() {
@@ -20,16 +25,18 @@ public class JMSMessager {
     }
 
     public void sendMessage(String queue, ApplicationEvent event) {
+        Connection connection = null;
+        Session session = null;
         try {
             // Create a ConnectionFactory
             ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(this.jmsUrl);
 
             // Create a Connection
-            Connection connection = connectionFactory.createConnection();
+            connection = connectionFactory.createConnection();
             connection.start();
 
             // Create a Session
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
             // Create the destination (Topic or Queue)
             Destination destination = session.createQueue(queue);
@@ -43,14 +50,21 @@ public class JMSMessager {
 
             // Tell the producer to send the message
             producer.send(message);
+            log.info("JMSMessanger - message send (" + message.toString() + ") to queue: " + queue);
 
-            // Clean up
-            session.close();
-            connection.close();
         } catch (Exception e) {
-            // TODO : dedicated logger needed
-            System.out.println("Caught: " + e);
+            log.error("JMSMessanger", e);
             e.printStackTrace();
+        } finally {
+            // Clean up
+            try {
+                if (session != null) session.close();
+                if (connection != null) connection.close();
+
+            } catch (JMSException e) {
+                log.error("JMSMessanger", e);
+                e.printStackTrace();
+            }
         }
     }
 }
